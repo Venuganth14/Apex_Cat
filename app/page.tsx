ebedac658bc65a89b0accd19bd5d59d07938fbfb
Venuganth14 <venuganth2001@gmail.com>
2025-05-06 09:37:31 +0530

    final

--- app/page.tsx
+++ app/page.tsx
@@ -29,7 +29,7 @@ export default function Home() {
           muted
           playsInline
         >
-          <source src="/sample.mp4" type="video/mp4" />
+          <source src="/sample1.mp4" type="video/mp4" />
           Your browser does not support the video tag.
         </video>
 
