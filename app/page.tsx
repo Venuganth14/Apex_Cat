30c3540b4c7e314e21850e0431f6bee3cb79155b
Venuganth14 <venuganth2001@gmail.com>
2025-05-06 23:47:04 +0530

    __

--- app/page.tsx
+++ app/page.tsx
@@ -12,11 +12,11 @@ export default function Home() {
     <div className="relative w-full">
       <div className="relative w-full h-screen overflow-hidden">
       <video autoPlay loop muted playsInline className="w-full h-full object-cover">
-  <source src="/sample1.mp4" type="video/mp4" />
+  <source src="/sample.mp4" type="video/mp4" />
   Your browser does not support the video tag.
 </video>
 
-        <div className="absolute inset-0 z-10 flex items-center px-8">
+        {/* <div className="absolute inset-0 z-10 flex items-center px-8">
           <div className="text-white">
             <h1 className="text-4xl md:text-6xl font-bold mb-4">
               Innovate. <br />
@@ -28,7 +28,7 @@ export default function Home() {
               smart automation, and digital excellence
             </p>
           </div>
-        </div>
+        </div> */}
       </div>
       <TechnologiesSection />
       <ServicesSection />
