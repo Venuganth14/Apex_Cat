3f7ca634030804ac27f50f4239d200e65896977e
Venuganth14 <venuganth2001@gmail.com>
2025-05-06 23:09:20 +0530

    __

--- app/page.tsx
+++ app/page.tsx
@@ -11,11 +11,11 @@ export default function Home() {
   return (
     <div className="relative w-full">
       <div className="relative w-full h-screen overflow-hidden">
-        <video className="absolute top-0 left-0 w-full h-full object-cover z-0 pointer-events-none" autoPlay loop muted playsInline>
-        <source src="/1.mp4" type="video/mp4" />
+      <video autoPlay loop muted playsInline className="w-full h-full object-cover">
+  <source src="/sample1.mp4" type="video/mp4" />
+  Your browser does not support the video tag.
+</video>
 
-          Your browser does not support the video tag.
-        </video>
         <div className="absolute inset-0 z-10 flex items-center px-8">
           <div className="text-white">
             <h1 className="text-4xl md:text-6xl font-bold mb-4">
